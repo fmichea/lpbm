8dedd2d4e883448174056bb60325cf1e8d33f076
Franck Michea <franck.michea@gmail.com>
2012-03-18 23:38:29 +0100

    Add: Category manager.

---
+++ lpbm/categories.py
@@ -0,0 +1,17 @@
+# categories.py - Category and category manager.
+
+class Category(object):
+    def __init__(self, name):
+        self.name = name
+        self.sub = dict()
+
+    def add_subcategory(self, cat):
+        if cat == []:
+            return
+        if cat[0] not in self.sub:
+            self.sub[cat[0]] = Category(cat[0])
+        self.sub[cat[0]].add_subcategory(cat[1:])
+
+class CategoryManager(Category):
+    def parse_category(self, cat):
+        self.add_subcategory(cat.split('|'))
