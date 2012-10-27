114e036020235cdae533a64a45d1f8509052f86a
Franck Michea <franck.michea@gmail.com>
2012-10-28 00:16:06 +0200

    remove dead code.

--- lpbm.py
+++ lpbm.py
@@ -17,34 +17,6 @@ import sys
 import lpbm.logging
 import lpbm.module_loader
 
-# FIXME: dead code
-#def clean_tree():
-#    # Cleaning output tree.
-#    if os.path.isdir(lpbm.constants.ROOT_OUTPUT):
-#        shutil.rmtree(lpbm.constants.ROOT_OUTPUT)
-#    if os.path.isdir(lpbm.constants.ROOT_OUTPUT_STYLESHEETS):
-#        shutil.rmtree(lpbm.constants.ROOT_OUTPUT_STYLESHEETS)
-#    if os.path.isdir(lpbm.constants.ROOT_OUTPUT_IMAGES):
-#        shutil.rmtree(lpbm.constants.ROOT_OUTPUT_IMAGES)
-#
-#def build_blog(ct_mgr, aut_mgr, art_mgr):
-#    lpbm.templates.render(art_mgr, aut_mgr, cat_mgr)
-#
-#    if os.path.isdir(lpbm.constants.ROOT_OUTPUT_IMAGES):
-#        shutil.rmtree(lpbm.constants.ROOT_OUTPUT_IMAGES)
-#    os.makedirs(lpbm.constants.ROOT_OUTPUT_IMAGES)
-#
-#    # Copy images using os.system, like a man.
-#    os.system("cp -R '%s'/* '%s'/* '%s' >/dev/null 2>&1" % (
-#        lpbm.constants.ROOT_IMAGES,
-#        lpbm.constants.ROOT_SRC_IMAGES,
-#        lpbm.constants.ROOT_OUTPUT_IMAGES
-#    ))
-#
-#def build_rss(cat_mgr, aut_mgr, art_mgr):
-#    lpbm.rss.render(art_mgr, aut_mgr, cat_mgr)
-#    pass
-
 _MODULES = dict()
 
 def main():
@@ -74,19 +46,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# FIXME: dead code.
-#    # Doing what is asked.
-#    if args.clean: clean_tree()
-#
-#    # Parsing articles, authors and generating categories.
-#    if not args.no_blog or not args.no_rss:
-#        cat_mgr = lpbm.categories.CategoryManager('Categories')
-#        aut_mgr = lpbm.authors.AuthorsManager()
-#        art_mgr = lpbm.articles.ArticlesManager(aut_mgr, cat_mgr)
-#
-#    # Generating what is asked.
-#    if not args.no_blog:
-#        build_blog(cat_mgr, aut_mgr, art_mgr)
-#    if not args.no_rss:
-#        build_rss(cat_mgr, aut_mgr, art_mgr)
