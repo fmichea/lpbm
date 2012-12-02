bfc2b26aba1bb9f90b04f2ad4cd84cbbbd1198cc
Franck Michea <franck.michea@gmail.com>
2012-12-02 07:22:07 +0100

    packaging lpbm.

---
+++ setup.py
@@ -0,0 +1,42 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    # General information.
+    name='lpbm',
+    description='Lightweight personal blog maker',
+    url='http://bitbucket.org/kushou/lpbm',
+
+    # Version information.
+    license='BSD',
+    version='2.0.0b1',
+
+    # Author.
+    author='Franck Michea',
+    author_email='franck.michea@gmail.com',
+
+    # File information.
+    install_requires=open('requirements.txt').readlines(),
+    packages=find_packages(exclude=['test', 'doc']),
+    package_data={'': ['*.css', '*.html']},
+    include_package_data=True,
+    entry_points = {
+        'console_scripts': [
+            'lpbm = lpbm.main:main',
+        ],
+    },
+
+    # Categories
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
