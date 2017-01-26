e377e03fcfc5f4fc8410c4917e7b72bf6fcc6587
Franck Michea <franck.michea@gmail.com>
2017-01-26 23:46:36 +0100

    Add essential functions to the path module (full_split, mkdir_p, remove)

--- lpbm/v3/lib/path.py
+++ lpbm/v3/lib/path.py
@@ -12,3 +12,30 @@ def initialize(ctx):
 def in_blog_join(*args):
     assert _EXEC_PATH is not None
     return os.path.join(_EXEC_PATH, *args)
+
+
+def full_split(path):
+    parts, head = [], None
+    while head != '':
+        head, tail = os.path.split(path)
+        parts.append(tail)
+        path = head
+    return parts
+
+
+def mkdir_p(path):
+    """
+    Emulates the behaviour of `mkdir -p` in shell (makes all the directories
+    of the path specified).
+    """
+    try:
+        os.makedirs(path)
+    except OSError:
+        pass
+
+
+def remove(path):
+    if os.path.isfile(path):
+        os.remove(path)
+    elif os.path.isdir(path):
+        os.rmdir(path)
---
+++ tests/v3/lib/test_path.py
@@ -0,0 +1,18 @@
+import os
+
+import lpbm.v3.lib.path as mod
+
+
+def test_remove_works_on_file(test_tempdir):
+    fname = os.path.join(test_tempdir, 'foo.txt')
+
+    with open(fname, 'w') as fd:
+        fd.write('fooooo')
+    mod.remove(fname)
+
+
+def test_remove_works_on_directory(test_tempdir):
+    fname = os.path.join(test_tempdir, 'foo')
+
+    mod.mkdir_p(fname)
+    mod.remove(fname)
