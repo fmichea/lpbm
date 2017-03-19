59d5439a87f408f163c8b13e0b7d9c7fcf46d252
Franck Michea <franck.michea@gmail.com>
2017-03-19 13:16:56 +0100

    Add listdir function to path library.

--- lpbm/v3/lib/path.py
+++ lpbm/v3/lib/path.py
@@ -42,3 +42,22 @@ def remove(path):
         os.rmdir(path)
     else:
         warnings.warn('got unknown file or unexpected file type.')
+
+
+def listdir(rootdir):
+    result = []
+    for root, dirs, files in os.walk(rootdir):
+        # Modifying the list in place to ignore all hidden directories.
+        ignored_dirs = set()
+        for idx, dirname in enumerate(dirs):
+            if dirname.startswith('.'):
+                ignored_dirs.add(idx)
+        for idx in ignored_dirs:
+            del dirs[idx]
+        # Also ignoring all the hidden files.
+        for filename in files:
+            if filename.startswith('.'):
+                continue
+            filepath = os.path.join(root, filename)
+            result.append(filepath[len(rootdir) + 1:])
+    return result
--- tests/v3/lib/test_path.py
+++ tests/v3/lib/test_path.py
@@ -21,3 +21,28 @@ def test_remove_works_on_directory(test_tempdir):
 def test_remove_warns_if_other_file_type(test_tempdir):
     fname = os.path.join(test_tempdir, 'foo')
     mod.remove(fname)
+
+
+def test_listdir_ignores_hidden_files_and_dirs(test_tempdir):
+    for fname in [
+        'a/b/.c/d',
+        'a/.we',
+        'b',
+    ]:
+        mod.mkdir_p(os.path.join(test_tempdir, fname))
+
+    for fname in [
+        'a/wde',
+        'a/b/.c/wqdgt',
+        'a/b/asdf',
+        'b/.wedwed',
+        'b/jdkdj',
+    ]:
+        with open(os.path.join(test_tempdir, fname), 'w') as fd:
+            fd.write('')
+
+    assert sorted(mod.listdir(test_tempdir)) == [
+        'a/b/asdf',
+        'a/wde',
+        'b/jdkdj',
+    ]
